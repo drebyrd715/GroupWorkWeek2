69571e2d4a97fe009b17233daebba5d9d0c2b1f7
drebyrd715 <drebyrd715@gmail.com>
2023-10-27 15:40:04 -0400

    fizz buzz answered

--- review.js
+++ review.js
@@ -9,14 +9,19 @@ for(let i = 0; i <= 20; i++){
 ////////////////////////////////
 // Get Even
 ////////////////////////////////
-for(let i = 0; i <= 200; i = i+2) {
-//     console.log(i)
- }
+// for(let i = 0; i <= 200; i = i+2) {
+//    console.log(i)
+//  }
 
 ////////////////////////////////
 // Fizz Buzz
 ////////////////////////////////
-
+for (let i = 0; i <= 100; i++){
+    if (i % 3 === 0) console.log("Fizz");
+   else if (i % 5 === 0) console.log("Buzz");
+   if (i % 3 === 0 && i % 5 === 0) console.log("FizzBuzz");
+   else console.log(i); 
+}
 
 ////////////////////////////////
 // Wild Wild Life
