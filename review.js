24ff0bc838dccfce7b65ddfa1f988b3bb1e05125
drebyrd715 <drebyrd715@gmail.com>
2023-10-27 15:58:59 -0400

    gameboy

--- review.js
+++ review.js
@@ -16,18 +16,34 @@ for(let i = 0; i <= 20; i++){
 ////////////////////////////////
 // Fizz Buzz
 ////////////////////////////////
-for (let i = 0; i <= 100; i++){
-    if (i % 3 === 0) console.log("Fizz");
-   else if (i % 5 === 0) console.log("Buzz");
-   if (i % 3 === 0 && i % 5 === 0) console.log("FizzBuzz");
-   else console.log(i); 
-}
+// for (let i = 0; i <= 100; i++){
+//     if (i % 3 === 0) console.log("Fizz");
+//    else if (i % 5 === 0) console.log("Buzz");
+//    if (i % 3 === 0 && i % 5 === 0) console.log("FizzBuzz");
+//    else console.log(i); 
+// }
 
 ////////////////////////////////
 // Wild Wild Life
 ////////////////////////////////
+// Plantee just had her birthday; change Plantee's array to reflect her being a year older.
+// Change Wolfy's hometown from "Yukon Territory" to "Gotham City".
+// Give D'Art a second hometown by adding "Hawkins"
+// Porgee decides that Wolfy can't be named "Wolfy" anymore. Remove "Wolfy" from the wolfyarray and replace it with "Gameboy".
+const wolfy = ["Wolfy", "wolf", 16, "Yukon Territory"]
+const sharky = ["Sharky", "shark", 20, "Left Coast"]
+const plantee = ["Plantee", "plant",  5000 , "Mordor"]
+const porgee = ["Porgee", "Porg", 186, "Ahch-To"]
+const dart = ["D'Art" , "Demogorgan Dog", 2, "Upside Down"]
 
-
+plantee[2] ++
+// console.log(plantee[2])
+wolfy [3] = "Gotham city"
+// console.log(wolfy[3])
+dart.push("Hawkins")
+// console.log(dart)
+wolfy [0] = "Gameboy"
+// console.log(wolfy)
 ////////////////////////////////
 // Yell at the Ninja Turtles
 ////////////////////////////////
